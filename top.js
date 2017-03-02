52c3667ac857a3913d1718f13fa064a299b769bf
athdot <athdot@gmail.com>
2017-03-02 14:47:35 -0700

    Update top.js

--- top.js
+++ top.js
@@ -1,2 +1,4 @@
-document.write("Hi")
-alert("Wazzap!")
+var myP = "Herro"
+document.getElementById("myP").style.fontSize = "xx-large";
+document.getElementById("myP").bold()
+document.getElementById("myP").fontcolor("green")
