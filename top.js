ed06eb4b006dbaef9a867ef06f2a1ce7ebe79b99
athdot <athdot@gmail.com>
2017-03-02 21:39:23 -0700

    Update top.js

--- top.js
+++ top.js
@@ -1,6 +1,6 @@
         var ifrm = document.createElement("iframe");
         ifrm.setAttribute("src", "https://athdot.github.io/test/window.html");
-        var scwid = (screen.width)*(9/10)
+        var scwid = (screen.width)*(19/20)
         ifrm.style.width = (scwid + "px");
         ifrm.style.height = "100px";
         document.body.appendChild(ifrm);
